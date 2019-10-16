f393329971b061276f3595b94cb7f60a4658cfcc
lr0929 <liurong9@126.com>
2019-10-16 11:48:33 +0800

    33

---
+++ src/cm/xinzhi/Test.java
@@ -0,0 +1,13 @@
+package cm.xinzhi;
+
+public class Test {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
